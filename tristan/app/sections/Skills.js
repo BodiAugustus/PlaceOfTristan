f964d4fbb45e0225c8097652f71398b2a2480685
BodiAugustus <tristanettles@gmail.com>
2024-02-25 10:57:53 -0500

    mobile

--- tristan/app/sections/Skills.js
+++ tristan/app/sections/Skills.js
@@ -17,6 +17,7 @@ const SkillsList = () => {
       custom-xs-640-skillsSecyion 
       custom-xs4-660-skillsSecyion
       custom-xs4-550-skillsSecyion 
+      custom-xs5-670-skillsSecyion
       custom-xs5-850-skillsSecyion
       custom-xs6-650-skillsSecyion
       custom-xs6-780-skillsSecyion
